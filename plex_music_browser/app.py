80278833a8fb0af6d9823b45528564d12d6a61c7
smartflutist661 <smartflutist661@gmail.com>
2026-03-08 22:24:03 -0400

    Proof-of-concept for plotting on main page

--- plex_music_browser/app.py
+++ plex_music_browser/app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sqlite3
+from datetime import datetime
 from pathlib import Path
 from sqlite3 import Connection
 from typing import (
@@ -8,6 +9,8 @@ from typing import (
     cast,
 )
 
+import pandas
+import plotly.express as px
 from dotenv import load_dotenv
 from flask import (
     Flask,
@@ -17,7 +20,6 @@ from flask.globals import request
 from flask.templating import render_template
 from flask.wrappers import Response
 
-from plex_music_browser.models.album import Album
 from plex_music_browser.models.datatable_request import (
     datatable_request_to_search_criteria,
     datatable_request_to_sort_criteria,
@@ -36,10 +38,15 @@ from plex_music_browser.queries.queries import (
 )
 from plex_music_browser.search import (
     IntSearchParam,
+    OneParameterCriterion,
     SearchColumn,
     SearchCondition,
     SearchCriteria,
-    TwoParameterCriterion,
+)
+from plex_music_browser.sort import (
+    SortColumn,
+    SortCriteria,
+    SortCriterion,
 )
 
 load_dotenv(Path(__file__).parents[1] / ".env")
@@ -77,7 +84,7 @@ def certbot(token: str) -> str:
     Route to answer a challenge from certbot
     for Let's Encrypt SSL certficate creation/renewal
     """
-    webroot = os.getenv("WEBROOT")
+    webroot = os.getenv("WEBROOT", "/var/www/html")
     if webroot is None:
         raise FileNotFoundError("WEBROOT not set in .env, unable to answer certbot challenge")
 
@@ -85,22 +92,18 @@ def certbot(token: str) -> str:
         return token_file.read()
 
 
-@APP.route("/")
-def index() -> str | Response:
-    now = datetime.now()
-    start_of_this_month = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-    end_of_last_month = start_of_this_month - timedelta(days=1)
-    start_of_last_month = end_of_last_month.replace(day=1)
+def generate_plots() -> str | Response:
+    this_year = datetime.today().year
+    start_time = datetime(this_year, 1, 1)
 
     search_criteria = SearchCriteria(
         basic_search_string=None,
         advanced_search={
             "AND": [
-                TwoParameterCriterion(
+                OneParameterCriterion(
                     SearchColumn("last_rated_at"),
-                    SearchCondition("between"),
-                    IntSearchParam(int(start_of_last_month.timestamp())),
-                    IntSearchParam(int(end_of_last_month.timestamp())),
+                    SearchCondition(">"),
+                    IntSearchParam(int(start_time.timestamp())),
                 )
             ]
         },
@@ -108,7 +111,7 @@ def index() -> str | Response:
     cur = get_db().cursor()
     filtered_items = get_items(
         search_criteria,
-        sort_criteria=None,
+        sort_criteria=SortCriteria([SortCriterion(SortColumn("last_rated_at"), "asc")]),
         query_type="albums",
         db_cursor=cur,
         artist_id=None,
@@ -120,7 +123,55 @@ def index() -> str | Response:
     if isinstance(filtered_items, Response):
         return filtered_items
 
-    return render_template("index.html", title="Home", plot="")
+    albums_df = pandas.DataFrame(item.model_dump() for item in filtered_items)
+    albums_df["month"] = albums_df["last_rated_at"].dt.strftime("%b")
+    grouped_by_month = (
+        albums_df.groupby(by=["month", "rating"])
+        .agg(
+            count=pandas.NamedAgg(column="album", aggfunc="count"),
+        )
+        .reset_index()
+    )
+    grouped_by_month["rating"] = grouped_by_month["rating"].astype(str)
+
+    # TODO: Add avg rating line plot overlay?
+    # TODO: Better color
+    fig1 = px.bar(
+        grouped_by_month,
+        title="Albums rated by month",
+        x="month",
+        y="count",
+        color="rating",
+        template="seaborn+presentation",  # type: ignore[arg-type] # presentation is an add-on
+        category_orders={
+            "month": [
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "June",
+                "July",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec",
+            ]
+        },
+        # hover_data="album",
+    )
+    # TODO: Loop through raw table grouped by month to create tabs(?) with datatable each
+    # TODO: Table by genre
+    return fig1.to_html(full_html=False, include_plotlyjs=False)
+
+
+@APP.route("/")
+def index() -> str | Response:
+    plots = generate_plots()
+    if isinstance(plots, Response):
+        return plots
+    return render_template("index.html", title="Home", plot=plots)
 
 
 @APP.route("/tracks")
